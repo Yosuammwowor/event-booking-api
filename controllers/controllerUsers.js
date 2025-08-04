f9e41924c0b85d88575b6aecaa28454a28c19a79
Yosua <yosuammwowor@gmail.com>
2025-08-04 10:25:52 +0800

    fix: add another error handling when adding new user through /users endpoint

--- controllers/controllerUsers.js
+++ controllers/controllerUsers.js
@@ -23,6 +23,11 @@ const addUsers = async (req, res) => {
 
   const { name, email, password } = req.body;
 
+  if (!name || !email || !password)
+    return res
+      .status(400)
+      .json({ message: "Please provide the necessary input needed!" });
+
   // check values data type
   if (
     !isValidString(name) ||
